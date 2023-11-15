0932e0fe8a819322fa3b595bfbd4926a84fe0427
NgynDT <NgynDT>
2023-11-15 17:25:52 +0000

    Pushed from Locofy Builder

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import BubbleTeeForm from "../components/bubble-tee-form";
+import styles from "./index.module.css";
+
+const TrangChiTitSnPhm = () => {
+  return (
+    <div className={styles.trangChiTitSnPhm}>
+      <div className={styles.trangChiTitSnPhm1}>
+        <BubbleTeeForm />
+      </div>
+    </div>
+  );
+};
+
+export default TrangChiTitSnPhm;
